6162eeddf4d8bbf71d77f079417c132c01eff008
Atul Varma <varmaa@gmail.com>
2018-01-02 00:36:48 -0500

    Add some structs and enums.

--- rl/easy21/src/main.rs
+++ rl/easy21/src/main.rs
@@ -2,6 +2,33 @@ extern crate rand;
 
 use rand::{Rng, SeedableRng, StdRng};
 
+type Reward = f32;
+
+enum Action {
+    Hit,
+    Stick
+}
+
+enum Color {
+    Red,
+    Black
+}
+
+struct State {
+    dealer: i32,
+    player: i32,
+}
+
+impl State {
+    fn new() -> Self {
+        State { dealer: 0, player: 0 }
+    }
+
+    fn step(self, action: Action) -> (Self, Reward) {
+        unimplemented!();
+    }
+}
+
 fn main() {
     let seed: &[_] = &[1, 2, 3, 4];
     let mut rng: StdRng = SeedableRng::from_seed(seed);
