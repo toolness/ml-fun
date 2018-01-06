175842d1646e4e31bfa888bff7cda3672a233d2b
Atul Varma <varmaa@gmail.com>
2018-01-06 10:20:57 -0500

    Add 'mc' subcommand via clap.

--- rl/easy21/README.md
+++ rl/easy21/README.md
@@ -1,5 +1,11 @@
 This is a Rust implementation of the [Easy21][] assignment from
 [David Silver's Reinforcement Learning class][class].
 
+## Quick start
+
+```
+cargo run --release -- help
+```
+
 [Easy21]: http://www0.cs.ucl.ac.uk/staff/d.silver/web/Teaching_files/Easy21-Johannes.pdf
 [class]: http://www0.cs.ucl.ac.uk/staff/d.silver/web/Teaching.html
--- rl/easy21/src/main.rs
+++ rl/easy21/src/main.rs
@@ -1,22 +1,38 @@
 extern crate rand;
+extern crate clap;
 
 mod game;
 mod montecarlo;
 
 use game::RngDeck;
 use rand::{SeedableRng, StdRng};
+use clap::{App, SubCommand};
 
-fn main() {
+fn run_monte_carlo() {
     let seed: &[_] = &[1, 2, 3, 4];
     let rng: StdRng = SeedableRng::from_seed(seed);
     let deck = RngDeck::new(rng);
     let mut control = montecarlo::Control::new(deck, rng);
 
-    const EPISODES: i32 = 30000;
+    const EPISODES: i32 = 30_000;
 
     println!("Playing {} episodes using Monte Carlo control...",
              EPISODES);
     control.play_episodes(EPISODES);
 
     control.print_optimal_value_fn();
+}
+
+fn main() {
+    let matches = App::new("easy21")
+      .subcommand(SubCommand::with_name("mc")
+        .about("runs monte carlo control"))
+      .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("mc") {
+        run_monte_carlo();
+    } else {
+        println!("Unknown command. Try running this program with '--help'.");
+        std::process::exit(1);
+    }
 }
