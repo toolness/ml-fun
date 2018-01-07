3571c8442485ca731dec879de78bfa76549277a8
Atul Varma <varmaa@gmail.com>
2018-01-06 21:59:55 -0500

    Add --episodes option.

--- rl/easy21/src/main.rs
+++ rl/easy21/src/main.rs
@@ -7,35 +7,50 @@ mod montecarlo;
 mod util;
 
 use rand::{SeedableRng, StdRng};
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 use gpi::Alg;
 
-fn run_monte_carlo() {
+fn run_monte_carlo(episodes: i32) {
     let seed: &[_] = &[1, 2, 3, 4];
     let rng: StdRng = SeedableRng::from_seed(seed);
     let deck = game::RngDeck::new(rng);
     let mc_alg = montecarlo::MonteCarlo::new();
     let mut gpi = gpi::Gpi::new(deck, rng, mc_alg);
 
-    const EPISODES: i32 = 30_000;
-
     println!("Performing GPI over {} episodes using Monte Carlo...",
-             EPISODES);
-    gpi.play_episodes(EPISODES);
+             episodes);
+    gpi.play_episodes(episodes);
 
     gpi.alg.print_optimal_values();
 }
 
+fn fail(msg: &str) {
+    eprintln!("{}", msg);
+    std::process::exit(1);
+}
+
 fn main() {
     let matches = App::new("easy21")
+      .arg(Arg::with_name("episodes")
+        .short("e")
+        .long("episodes")
+        .help("number of episodes to play")
+        .default_value("30000")
+        .takes_value(true))
       .subcommand(SubCommand::with_name("mc")
         .about("runs monte carlo control"))
       .get_matches();
 
+    let episodes = matches.value_of("episodes")
+      .unwrap().parse::<i32>().unwrap_or(0);
+
+    if episodes <= 0 {
+        fail("Episodes must be a number greater than 0.");
+    }
+
     if let Some(_) = matches.subcommand_matches("mc") {
-        run_monte_carlo();
+        run_monte_carlo(episodes);
     } else {
-        println!("Unknown command. Try running this program with '--help'.");
-        std::process::exit(1);
+        fail("Unknown command. Try running this program with '--help'.");
     }
 }
